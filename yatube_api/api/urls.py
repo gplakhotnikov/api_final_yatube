11d7df9272a005f5dd4f5dda5a76eac51474f468
Grigory Plakhotnikov <gregorio@inbox.ru>
2022-05-14 21:34:23 -0300

    Before 2 review

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -3,9 +3,9 @@ from rest_framework.routers import SimpleRouter
 from api.views import PostViewSet, CommentViewSet, GroupViewSet, FollowViewSet
 
 router = SimpleRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register('follow', FollowViewSet)
+router.register('posts', PostViewSet, basename='posts')
+router.register('groups', GroupViewSet, basename='groups',)
+router.register('follow', FollowViewSet, basename='follow',)
 router.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet, basename='comments',)
